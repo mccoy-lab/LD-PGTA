68db206a011d9531968f7d9b1a14cfc0e843e131
Daniel Ariad <daniel@ariad.org>
2020-09-13 13:06:50 -0400

    Nightly build

---
+++ SIMULATE_OBS_TAB.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SIMULATE_OBS_TAB
+
+Simulates an observation table, obs_tab of a haploid, using phased genotypes from vcf files.
+
+Daniel Ariad (daniel@ariad.org)
+Aug 31, 2020
+"""
+import pickle, sys, os, time, argparse
+
+def get_alleles_from_bcftools(vcf_file,chr_id,sample_id,bcftools_dir):
+    """ Runs bcftools as a subprocess and returns the output """
+    
+    ins_dir = bcftools_dir if bcftools_dir=='' or bcftools_dir[-1]=='/' else bcftools_dir+'/'
+    cmd1 = '%sbcftools view %s --samples %s --targets %s --phased --exclude-types indels,mnps,ref,bnd,other --output-type v' % (ins_dir,vcf_file,sample_id,chr_id[3:])
+    cmd2 = '%sbcftools query -f \'%%CHROM\\t%%POS\\t[%%TGT]\\n\' - > AgT4Fa1z.tmp' % ins_dir
+    run = cmd1 + ' | ' + cmd2
+    print('Calling bcftools:\n%s' % run)
+    os.system(run)
+    return 0 
+
+
+def read_impute2(impute2_filename,**kwargs):
+    """ Reads an IMPUTE2 file format (SAMPLE/LEGEND/HAPLOTYPE) and builds a list
+        of lists, containing the dataset. """
+
+    filetype = kwargs.get('filetype', None)
+    with open(impute2_filename, 'r') as impute2_in:
+        if filetype == 'legend':
+            impute2_in.readline()   # Bite off the header
+            def parse(x):
+                y=x.strip().split()
+                y[0] = 'chr'+y[0].split(':')[0]
+                y[1]=int(y[1])
+                return y
+        elif filetype == 'hap':
+            def parse(x):
+                return [i=='1' for i in x.strip().split()]
+        else:
+            def parse(x):
+                return x.strip().split() # Trailing whitespaces stripped from the ends of the string. Then it splits the string into a list of words.
+
+        impute2_tab = [parse(line) for line in impute2_in]
+    return impute2_tab
+
+def parse(x):
+    """ Parses the output of the bcftools query. """ 
+    
+    line = x.strip().split('\t')
+    result = ['chr'+line[0],int(line[1])]+line[-1].strip().split('|')
+    return result
+
+
+def main(vcf_filename,leg_filename,chr_id,sample_id,bcftools_dir):
+    
+    a = time.time()
+    
+    get_alleles_from_bcftools(vcf_filename,chr_id,sample_id,bcftools_dir)
+    
+    with open('AgT4Fa1z.tmp', 'r') as txtfile:
+        tab = [parse(line) for line in txtfile]
+    os.remove('AgT4Fa1z.tmp')
+
+    REF = {pos:ref for chr_id,pos,ref,alt in tab}
+    ALT = {pos:alt for chr_id,pos,ref,alt in tab}
+
+    impute2_tab = read_impute2(leg_filename,filetype='legend')
+
+    obs_tab1 = tuple((pos, impute2_index, 'XXX', REF[pos])
+                     for impute2_index,(chr_id,pos,ref,alt) in enumerate(impute2_tab)
+                     if pos in REF)
+
+    obs_tab2 = tuple((pos, impute2_index, 'XXX', ALT[pos])
+                     for impute2_index,(chr_id,pos,ref,alt) in enumerate(impute2_tab)
+                     if pos in ALT)
+
+    info = {'chr_id': chr_id}
+    
+    with open(sample_id+'A.%s.hg38.obs.p' % chr_id, 'wb') as binfile:
+        pickle.dump(obs_tab1, binfile, protocol=4)
+        pickle.dump(info, binfile, protocol=4)
+
+    with open(sample_id+'B.%s.hg38.obs.p' % chr_id, 'wb') as binfile:
+        pickle.dump(obs_tab2, binfile, protocol=4)
+        pickle.dump(info, binfile, protocol=4)
+
+    b = time.time()
+    print('Done in %.3f sec.' % ((b-a)))
+    
+    return 0
+
+if __name__ == "__main__": 
+    
+    parser = argparse.ArgumentParser(
+        description='Simulates two observation tables of a haploid, using phased genotypes from vcf files.'
+                    'Each observation table includes SNP positions, alleles in a genotype of an individual'
+                    'and their corresponding line number within the IMPUTE2 legend file.')
+    parser.add_argument('vcf', metavar='vcf_filename', type=str, 
+                        help='VCF file')
+    parser.add_argument('impute_leg', metavar='legend_filename', type=str, 
+                        help='IMPUTE2 legend file')
+    parser.add_argument('chr_id', metavar='chromosomeID', type=str,
+                        help='Chromosome ID')
+    parser.add_argument('sample_id', metavar='sampleID', type=str,
+                        help='Sample ID')
+    parser.add_argument('-s', '--bcftools-dir', type=str,
+                        default='', metavar='SAMTOOLS_DIR',
+                        help='The directory where bcftools are installed.')
+    
+    args = parser.parse_args()
+    sys.exit(main(**vars(args)))
+
+def test():
+    bcftools_dir = ''
+    sample_id = 'HG00097'
+    chr_id = 'chr21'
+    #leg_filename = '../build_reference_panel/ref_panel.ALL.hg38.BCFtools/chr21_ALL_panel.legend'
+    #leg_filename = '../build_reference_panel/ref_panel.Ashkenazi.hg38.BCFtools/chr21_Ashkenazi_panel.legend'
+    #leg_filename = '../build_reference_panel/ref_panel.EUR.hg38.BCFtools/chr21_EUR_panel.legend'
+    leg_filename = '../build_reference_panel/ref_panel.EUR.hg38.BCFtools/%s_EUR_panel.legend' % chr_id
+    #leg_filename = '../build_reference_panel/ref_panel.TEST.hg38.BCFtools/chr21_TEST_panel.legend'
+
+    vcf_filename = '../vcf_phase3_hg38_v2/ALL.%s.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz' % chr_id
+    return main(vcf_filename,leg_filename,chr_id,sample_id,bcftools_dir)
