1039b40c22078abb3a18dbbcaddc93d716568168
Daniel Ariad <daniel@ariad.org>
2020-12-30 12:37:18 -0500

    Daily build

--- BUILD_SIMULATED_SEQUENCES.py
+++ BUILD_SIMULATED_SEQUENCES.py
@@ -13,6 +13,7 @@ from random import sample, choices, seed
 from multiprocessing import Process
 from MIX_HAPLOIDS import MixHaploids_wrapper
 from SIMULATE_HAPLOIDS import main as simulate
+from os import remove
 
 def read_ref(filename):
     with open(filename, 'r') as data_in:
@@ -79,6 +80,7 @@ def main(depth,sp,chr_id,read_length,min_reads,max_reads):
     func = (eval(f"lambda: make_simulated_obs_tab('{a:s}', '{sp:s}', '{chr_id:s}', '{work_dir:s}')") for a in A)
     runInParallel(*func)
     filenames = MixHaploids_wrapper(f'{work_dir:s}{C[0]:s}.{chr_id:s}.hg38.obs.p', f'{work_dir:s}{C[1]:s}.{chr_id:s}.hg38.obs.p', f'{work_dir:s}{C[2]:s}.{chr_id:s}.hg38.obs.p', read_length=read_length, depth=depth, scenarios=('monosomy','disomy','SPH','BPH'),output_dir=work_dir)
+    remove(f'{work_dir:s}{C[0]:s}.{chr_id:s}.hg38.obs.p'); remove(f'{work_dir:s}{C[1]:s}.{chr_id:s}.hg38.obs.p'); remove(f'{work_dir:s}{C[2]:s}.{chr_id:s}.hg38.obs.p')
     func2 = (eval(f"lambda: aneuploidy_test_demo('{f:s}','{chr_id:s}','{sp:s}','MODELS/MODELS16.p',{min_reads:d},{max_reads:d},'{work_dir:s}')") for f in filenames)
     runInParallel(*func2)
     return 0
@@ -90,9 +92,10 @@ if __name__ == "__main__":
     ###chr_id='chr21'
     read_length = 36
     min_reads,max_reads = 3,5
-    for n in [*range(1,23)]+['X']:
-        chr_id = 'chr' + str(n)
-        main(depth,sp,chr_id,read_length,min_reads,max_reads)
+    for _ in range(100):
+        for n in [*range(1,23)]+['X']:
+            chr_id = 'chr' + str(n)
+            main(depth,sp,chr_id,read_length,min_reads,max_reads)
     pass
 else:
     print("The module BUILD_SIMULATED_SEQUENCES was imported.")
