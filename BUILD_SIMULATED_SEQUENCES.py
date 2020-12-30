118274108d43d65e979567f60c86c346e0a9db6d
Daniel Ariad <daniel@ariad.org>
2020-12-30 01:25:58 -0500

    Daily build

--- BUILD_SIMULATED_SEQUENCES.py
+++ BUILD_SIMULATED_SEQUENCES.py
@@ -5,14 +5,14 @@
 BUILD_SIMULATED_SEQUENCES
 
 Daniel Ariad (daniel@ariad.org)
-Nov 18, 2020
+Dec 30, 2020
 
 """
 import time, pickle, re, sys
 from random import sample, choices, seed
 from multiprocessing import Process
-from MIX_HAPLOIDS import MixHaploids2
-from SIMULATE_OBS_TAB import main as simulate
+from MIX_HAPLOIDS import MixHaploids_wrapper
+from SIMULATE_HAPLOIDS import main as simulate
 
 def read_ref(filename):
     with open(filename, 'r') as data_in:
@@ -58,11 +58,7 @@ def make_simulated_obs_tab(sample_id,sp,chr_id,output_dir):
     bcftools_dir = '' #'../bcftools-1.10.2/bin'
     #sample_id = 'HG00096'
     #chr_id = 'chr21'
-    #leg_filename = '../build_reference_panel/ref_panel.ALL.hg38.BCFtools/chr21_ALL_panel.legend'
-    #leg_filename = '../build_reference_panel/ref_panel.Ashkenazi.hg38.BCFtools/chr21_Ashkenazi_panel.legend'
-    #leg_filename = '../build_reference_panel/ref_panel.EUR.hg38.BCFtools/chr21_EUR_panel.legend'
-    leg_filename = f'../build_reference_panel/ref_panel.{sp:s}.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.legend'
-    #leg_filename = '../build_reference_panel/ref_panel.TEST.hg38.BCFtools/chr21_TEST_panel.legend'
+    leg_filename = f'../build_reference_panel/{sp:s}_panel.hg38.BCFtools/{chr_id:s}_{sp:s}_panel.legend'
     vcf_filename = f'../vcf_phase3_hg38_v2/ALL.{chr_id:s}.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz'
     #output_dir  = f'results_{sp:s}/'
     return simulate(vcf_filename,leg_filename,chr_id,sample_id,bcftools_dir,output_dir=output_dir)
@@ -82,20 +78,22 @@ def main(depth,sp,chr_id,read_length,min_reads,max_reads):
     #for a in A: make_simulated_obs_tab(a,sp,chr_id,work_dir)
     func = (eval(f"lambda: make_simulated_obs_tab('{a:s}', '{sp:s}', '{chr_id:s}', '{work_dir:s}')") for a in A)
     runInParallel(*func)
-    MixHaploids2(f'{work_dir:s}{C[0]:s}.chr21.hg38.obs.p', f'{work_dir:s}{C[1]:s}.chr21.hg38.obs.p', f'{work_dir:s}{C[2]:s}.chr21.hg38.obs.p', read_length=read_length, depth=depth, output_dir=work_dir, recombination_spots=[0.00,1.00])
-    filenames = (f'mixed3haploids.X{depth:.2f}.{C[0]:s}.{C[1]:s}.{C[2]:s}.chr21.recomb.{i:.2f}.obs.p' for i in (0,1))
+    MixHaploids_wrapper(f'{work_dir:s}{C[0]:s}.{chr_id:s}.hg38.obs.p', f'{work_dir:s}{C[1]:s}.{chr_id:s}.hg38.obs.p', f'{work_dir:s}{C[2]:s}.{chr_id:s}.hg38.obs.p', read_length=read_length, depth=depth, scenarios=('monosomy','disomy','SPH','BPH'),output_dir=work_dir, recombination_spots=[0])
+    filenames = (f'mixed3haploids.X{depth:.2f}.{C[0]:s}.{C[1]:s}.{C[2]:s}.{chr_id:s}.recomb.{i:.2f}.obs.p' for i in (0,1))
     func2 = (eval(f"lambda: aneuploidy_test_demo('{work_dir:s}{f:s}','{chr_id:s}','{sp:s}','MODELS/MODELS16.p',{min_reads:d},{max_reads:d},'{work_dir:s}')") for f in filenames)
     runInParallel(*func2)
     return 0
 
 
 if __name__ == "__main__":
-    depth=0.1
+    depth=0.01
     sp='EUR'
     chr_id='chr21'
-    read_length = 35
-    min_reads,max_reads = 3,14
-    main(depth,sp,chr_id,read_length,min_reads,max_reads)
+    read_length = 36
+    min_reads,max_reads = 3,5
+    for n in [*range(1,23)]+['X']:
+        chr_id = 'chr' + str(n)
+        main(depth,sp,chr_id,read_length,min_reads,max_reads)
     #for i in range(20):
     #    runInParallel(*(main for _ in range(12)),args=(depth,sp,chr_id,read_length,min_reads,max_reads))
     #depth=0.02
