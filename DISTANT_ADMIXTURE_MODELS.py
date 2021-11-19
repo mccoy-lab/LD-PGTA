4a5b7009f19755c407bd12d11d587b7f2072229b
Daniel Ariad <daniel@ariad.org>
2021-11-19 18:15:42 -0500

    Now support any number of populations

--- DISTANT_ADMIXTURE_MODELS.py
+++ DISTANT_ADMIXTURE_MODELS.py
@@ -27,8 +27,7 @@ from itertools import combinations
 leg_tuple = collections.namedtuple('leg_tuple', ('chr_id', 'pos', 'ref', 'alt')) #Encodes the rows of the legend table
 sam_tuple = collections.namedtuple('sam_tuple', ('sample_id', 'group1', 'group2', 'sex')) #Encodes the rows of the samples table
 obs_tuple = collections.namedtuple('obs_tuple', ('pos', 'read_id', 'base')) #Encodes the rows of the observations table
-admix_tuple = collections.namedtuple('admix_tuple', ('group2', 'proportion'))
-
+spanel_tuple = collections.namedtuple('spanel_tuple', ('group2', 'flag', 'hap_number', 'proportion')) #Encodes the rows of the observations table
 try:
     from gmpy2 import popcount
 except ModuleNotFoundError:
@@ -43,7 +42,7 @@ class distant_admixture:
     observed alleles under various statistical models (monosomy, disomy, SPH
     and BPH). """
 
-    def __init__(self, obs_tab, leg_tab, hap_tab, sam_tab, models_dict, total_number_of_haplotypes, admixture):
+    def __init__(self, obs_tab, leg_tab, hap_tab, sam_tab, models_dict, total_number_of_haplotypes, ancestral_makeup):
         """ Initialize the attributes of the class. """
 
         if len(leg_tab)!=len(hap_tab):
@@ -51,30 +50,43 @@ class distant_admixture:
 
         if total_number_of_haplotypes!=2*len(sam_tab):
             raise Exception('Error: the number of diploid samples in the SAMPLE file differ from the number of haplotypes in the HAP file.')
-
+        
+        if sum(ancestral_makeup.values())<0.999:
+            raise Exception('Error: ancestry proportions must sum to one.')
+            
         self.total_number_of_haplotypes_in_reference_panel = total_number_of_haplotypes
+        self.sub_panels = self.extract_subpanels(sam_tab)
+        self.ancestral_makeup = ancestral_makeup #Keys correspond to group2 names and values to haplotype proportions.
+        
+        for group2 in ancestral_makeup:
+            if group2 not in self.sub_panels: 
+                raise Exception('Error: %s is missing from the reference panel.' % group2)
+        
+        self.proportions = tuple(spanel_tuple(group2, *self.sub_panels[group2], proportion) for group2, proportion in ancestral_makeup.items())
         self.models_dict = models_dict
         self.hap_dict, self.fraction_of_matches = self.build_hap_dict(obs_tab, leg_tab, hap_tab)
-        self.flags, self.num_of_hap_in_ref_subpanel, self.name2id = self.subpanels(sam_tab)
-        self.admixture = admixture #The first element in the tuple is the group2 name and second is the haplotype proportion.
-        self.proportions = {self.name2id[admixture.group2]: admixture.proportion, 1-self.name2id[admixture.group2]: 1-admixture.proportion}
-        ##########print(str({ancestry: admixture.proportion if ancestry==admixture.group2 else 1-admixture.proportion for ancestry in self.name2id})[1:-1])
 
-    def subpanels(self, sam_tab):
+    def extract_subpanels(self, sam_tab):
         """ Differentiates between the two groups that compose the reference
         panel. Then, all the haplotypes that are associated with each group are
         flagged using a binary representation marks and counted. """
 
-        differentiate = [row.group2 == sam_tab[0].group2 for row in sam_tab for i in (1,2)]
-        flag0 = sum(v<<i for i, v in enumerate(reversed(differentiate)))
-        flag1 = flag0 ^ ((1 << self.total_number_of_haplotypes_in_reference_panel) - 1)
-        flags = (flag0, flag1)
-        name2id = {sam_tab[0].group2:0,
-                   sam_tab[differentiate[::2].index(False,1)].group2: 1}
-        N0 = differentiate.count(True)
-        N1 = self.total_number_of_haplotypes_in_reference_panel - N0
-        number_of_haplotypes_in_reference_subpanel = (N0,N1)
-        return flags, number_of_haplotypes_in_reference_subpanel, name2id
+        groups = list({j.group2 for j in sam_tab})
+        flags = [] #occupation representation of samples that are asscoiated with a group.
+        N = [] #number of haplotypes in reference subpanel.
+        for group2 in groups[:-1]:
+            differentiate = [row.group2 == group2 for row in sam_tab for i in (1,2)]
+            N.append(differentiate.count(True))
+            flag = sum(v<<i for i, v in enumerate(reversed(differentiate)))
+            flags.append(flag)
+        
+        last_N = self.total_number_of_haplotypes_in_reference_panel - sum(N)
+        N.append(last_N)
+        last_flag = sum(flags) ^ ((1 << self.total_number_of_haplotypes_in_reference_panel) - 1)
+        flags.append(last_flag)
+        
+        result = {group2: (flag, hap_number) for group2, flag, hap_number in zip(groups,flags,N)}
+        return result
 
     def build_hap_dict(self, obs_tab, leg_tab, hap_tab):
         """ Returns a dictionary that lists SNP alleles and gives their
@@ -131,7 +143,11 @@ class distant_admixture:
 
         return hap
 
-    def joint_frequencies_combo(self, *alleles, group2_id, normalize):
+    def effective_joint_frequency(self, hap):
+        """ The probability to draw a haplotype from one of the groups2. """
+        return sum(i.proportion * popcount(hap & i.flag) / i.hap_number for i in self.proportions)
+
+    def joint_frequencies_combo(self, *alleles):
         """ Based on the reference panel, it calculates joint frequencies of
             observed alleles. The function arguments are alleles, that is,
             tuples of position and base, e.g., (100,'T'), (123, 'A') and
@@ -145,28 +161,22 @@ class distant_admixture:
             arguments can also include haplotypes, that is, tuples of alleles;
             Haplotypes are treated in the same manner as alleles. """
 
-        flag = self.flags[group2_id]
         hap = self.intrenal_hap_dict(*alleles)
 
-        result = {c: popcount(A&flag) for c,A in hap.items()}
+        result = {c: self.effective_joint_frequency(A) for c,A in hap.items()}
 
         for C in combinations(hap, 2):
-            result[C[0]|C[1]] = popcount(hap[C[0]]&hap[C[1]]&flag)
+            result[C[0]|C[1]] = self.effective_joint_frequency(hap[C[0]]&hap[C[1]])
 
         for C in combinations(hap, 3):
-            result[C[0]|C[1]|C[2]] = popcount(hap[C[0]]&hap[C[1]]&hap[C[2]]&flag)
+            result[C[0]|C[1]|C[2]] = self.effective_joint_frequency(hap[C[0]]&hap[C[1]]&hap[C[2]])
 
         for r in range(4,len(alleles)):
             for C in combinations(hap, r):
-                result[sum(C)] = popcount(reduce(and_,itemgetter(*C)(hap))&flag)
+                result[sum(C)] = self.effective_joint_frequency(reduce(and_,itemgetter(*C)(hap)))
 
         if len(alleles)>=4:
-            result[sum(hap.keys())] = popcount(reduce(and_,hap.values())&flag)
-
-        if normalize:
-            N = self.num_of_hap_in_ref_subpanel[group2_id]
-            result = {k: v/N for k,v in result.items()}
-
+            result[sum(hap.keys())] = self.effective_joint_frequency(reduce(and_,hap.values()))
         return result
 
     def likelihoods(self, *alleles):
@@ -175,44 +185,39 @@ class distant_admixture:
         and BPH. """
 
         models = self.models_dict[len(alleles)]
-        F = self.joint_frequencies_combo(*alleles, group2_id=0, normalize=True)
-        G = self.joint_frequencies_combo(*alleles, group2_id=1, normalize=True)
-
-        m = self.proportions[0] #The probability to draw a haplotype that is associated with group2_id=0.
-        n = self.proportions[1] #The probability to draw a haplotype that is associated with group2_id=1.
+        
+        F = self.joint_frequencies_combo(*alleles)
 
         ### BPH ###
         (((A0, A1),((B0,),)),) = models['BPH'][1].items()
 
-        BPH = (A0 / A1)  * (m * F[B0] + n * G[B0])
+        BPH = (A0 / A1)  * F[B0]
 
 
-        BPH += sum( sum((m * F[B0] + n * G[B0]) * (m * F[B1] + n * G[B1]) for (B0, B1) in C) * A0 / A1
+        BPH += sum( sum(F[B0] * F[B1] for (B0, B1) in C) * A0 / A1
                    for (A0, A1), C in models['BPH'][2].items())
 
         if len(alleles)>2:
-            BPH += sum( sum((m * F[B0] + n * G[B0]) * sum( (m * F[B1] + n * G[B1]) * (m * F[B2] + n * G[B2])
-                  for (B1, B2) in C[B0]) for B0 in C) * A0 / A1
+            BPH += sum( sum(F[B0] * sum( F[B1] * F[B2] for (B1, B2) in C[B0]) for B0 in C) * A0 / A1
                      for (A0, A1), C in models['BPH'][3].items())
 
         ### SPH ###
         (((A0, A1),((B0,),)),) = models['SPH'][1].items()
-        SPH = (A0 / A1)  * (m * F[B0] + n * G[B0])
+        SPH = (A0 / A1) * F[B0]
 
-        SPH += sum( sum((m * F[B0] + n * G[B0]) * (m * F[B1] + n * G[B1]) for (B0, B1) in C) * A0 / A1
+        SPH += sum( sum(F[B0] * F[B1] for (B0, B1) in C) * A0 / A1
                    for (A0, A1), C in models['SPH'][2].items())
-
 
         ### DIPLOIDY ###
         (((A0, A1),((B0,),)),) = models['DISOMY'][1].items()
-        DISOMY = (A0 / A1)  * (m * F[B0] + n * G[B0])
+        DISOMY = (A0 / A1)  * F[B0]
 
-        DISOMY += sum( sum( (m * F[B0] + n * G[B0]) * (m * F[B1] + n * G[B1]) for (B0, B1) in C) * A0 / A1
+        DISOMY += sum( sum( F[B0] * F[B1] for (B0, B1) in C) * A0 / A1
                    for (A0, A1), C in models['DISOMY'][2].items())
 
         ### MONOSOMY ###
         ((B0,),) = models['MONOSOMY'][1][(1,1)]
-        MONOSOMY = m * F[B0] + n * G[B0]
+        MONOSOMY = F[B0]
 
         result = (MONOSOMY, DISOMY, SPH, BPH)
         return result
@@ -221,12 +226,8 @@ class distant_admixture:
         """ Calculates the likelihood to observe two alleles/haplotypes
         under four scenarios, namely, monosomy, disomy, SPH and BPH. """
 
-        m = self.proportions[0] #The probability to draw a haplotype that is associated with group2_id=0.
-        n = self.proportions[1] #The probability to draw a haplotype that is associated with group2_id=1.
-
-        F = self.joint_frequencies_combo(*alleles, group2_id=0, normalize=True)
-        G = self.joint_frequencies_combo(*alleles, group2_id=1, normalize=True)
-        a, b, ab = m*F[1]+n*G[1] , m*F[2]+n*G[2], m*F[3]+n*G[3] #The probability of a haplotype to be associated with group2_id=0 is m.
+        F = self.joint_frequencies_combo(*alleles)
+        a, b, ab = F[1] , F[2], F[3] #The probability of a haplotype to be associated with group2_id=0 is m.
 
         BPH = (ab+2*a*b)/3 #The likelihood of three unmatched haplotypes.
         SPH = (5*ab+4*a*b)/9 #The likelihood of two identical haplotypes out three.
@@ -239,12 +240,8 @@ class distant_admixture:
         """ Calculates the likelihood to observe three alleles/haplotypes
         under four scenarios, namely, monosomy, disomy, SPH and BPH. """
 
-        m = self.proportions[0] #The probability to draw a haplotype that is associated with group2_id=0.
-        n = self.proportions[1] #The probability to draw a haplotype that is associated with group2_id=1.
-
-        F = self.joint_frequencies_combo(*alleles, group2_id=0, normalize=True)
-        G = self.joint_frequencies_combo(*alleles, group2_id=1, normalize=True)
-        a, b, ab, c, ac, bc, abc = (m*F[i]+n*G[i] for i in range(1,8))
+        F = self.joint_frequencies_combo(*alleles)
+        a, b, ab, c, ac, bc, abc = (F[i] for i in range(1,8))
 
         BPH = (abc+2*(ab*c+ac*b+bc*a+a*b*c))/9 #The likelihood of three unmatched haplotypes.
         SPH = abc/3+2*(ab*c+ac*b+bc*a)/9  #The likelihood of two identical haplotypes out three.
@@ -257,12 +254,8 @@ class distant_admixture:
         """ Calculates the likelihood to observe four alleles/haplotypes
         under four scenarios, namely, monosomy, disomy, SPH and BPH. """
 
-        m = self.proportions[0] #The probability to draw a haplotype that is associated with group2_id=0.
-        n = self.proportions[1] #The probability to draw a haplotype that is associated with group2_id=1.
-
-        F = self.joint_frequencies_combo(*alleles, group2_id=0, normalize=True)
-        G = self.joint_frequencies_combo(*alleles, group2_id=1, normalize=True)
-        a, b, ab, c, ac, bc, abc, d, ad, bd, abd, cd, acd, bcd, abcd = (m*F[i]+n*G[i] for i in range(1,16))
+        F = self.joint_frequencies_combo(*alleles)
+        a, b, ab, c, ac, bc, abc, d, ad, bd, abd, cd, acd, bcd, abcd = (F[i] for i in range(1,16))
 
         BPH = (abcd+2*(ab*c*d+a*bd*c+a*bc*d+ac*b*d+a*b*cd+ad*b*c+abc*d+a*bcd+acd*b+abd*c+ab*cd+ad*bc+ac*bd))/27  #The likelihood of three unmatched haplotypes.
         SPH = (17*abcd+10*(abc*d+bcd*a+acd*b+abd*c)+8*(ab*cd+ad*bc+ac*bd))/81  #The likelihood of two identical haplotypes out three.
@@ -322,6 +315,7 @@ def wrapper_of_distant_admixture_for_debugging(obs_filename,leg_filename,hap_fil
     with open_model(models_filename, 'rb') as model_in:
         models_dict = pickle.load(model_in)
 
+    ###return obs_tab, leg_tab, hap_tab, sam_tab, models_dict, total_number_of_haplotypes, admixture
     return distant_admixture(obs_tab, leg_tab, hap_tab, sam_tab, models_dict, total_number_of_haplotypes, admixture)
 
 if __name__ != "__main__":
@@ -346,18 +340,14 @@ else:
     leg_filename = 'test/test.leg.p'
     sam_filename = 'test/test.sam.p'
     models_filename = 'MODELS/MODELS16.p'
-    admixture = admix_tuple('group0',0.8)
+    admixture = {'group0': 1}
 
     A = wrapper_of_distant_admixture_for_debugging(obs_filename,leg_filename,hap_filename,sam_filename,models_filename,admixture)
 
     alleles = tuple(A.hap_dict.keys())
 
-    ###frequencies0 = lambda *x: {bin(a)[2:]:b for a,b in A.joint_frequencies_combo(*x,group2_id=0,normalize=True).items()}
-    ###frequencies1 = lambda *x: {bin(a)[2:]:b for a,b in A.joint_frequencies_combo(*x,group2_id=1,normalize=True).items()}
-    effective_frequencies = lambda *x: {bin(k0)[2:]: A.proportions[0] * v0 + A.proportions[1] * v1  
-                                       for (k0,v0),(k1,v1) in zip(A.joint_frequencies_combo(*x,group2_id=0,normalize=True).items(),
-                                                                  A.joint_frequencies_combo(*x,group2_id=1,normalize=True).items()) 
-                                                                       if k0==k1}
+    frequencies = lambda *x: {bin(a)[2:]:b for a,b in A.joint_frequencies_combo(*x).items()}
+
     likelihoods = A.likelihoods
     likelihoods2 = A.likelihoods2
     likelihoods3 = A.likelihoods3
@@ -365,28 +355,30 @@ else:
 
     random.seed(a=2021, version=2)
     x = random.randrange(len(alleles)-16)
-    haplotypes = (alleles[x:x+4],alleles[x+4:x+8],alleles[x+8:x+12],alleles[x+12:x+16]) 
+    haplotypes = (alleles[x:x+4],alleles[x+4:x+8],alleles[x+8:x+12],alleles[x+12:x+16])
+
     print('-----joint_frequencies_combo-----')
-    print(effective_frequencies(alleles[x+0]))
-    print(effective_frequencies(*alleles[x:x+4]))
+    print(frequencies(alleles[x+0]))
+    print(frequencies(*alleles[x:x+4]))
+ 
     print('-----likelihoods4-haplotypes-----')
     print(haplotypes)
-    print(effective_frequencies(*haplotypes))
+    print(frequencies(*haplotypes))
     print(likelihoods(*haplotypes))
     print(likelihoods4(*haplotypes))
     print('-----likelihoods2-----')
     print(alleles[x:x+2])
-    print(effective_frequencies(*alleles[x:x+2]))
+    print(frequencies(*alleles[x:x+2]))
     print(likelihoods(*alleles[x:x+2]))
     print(likelihoods2(*alleles[x:x+2]))
     print('-----likelihoods3-----')
     print(alleles[x:x+3])
-    print(effective_frequencies(*alleles[x:x+3]))
+    print(frequencies(*alleles[x:x+3]))
     print(likelihoods(*alleles[x:x+3]))
     print(likelihoods3(*alleles[x:x+3]))
     print('-----likelihoods4-----')
     print(alleles[x:x+4])
-    print(effective_frequencies(*alleles[x:x+4]))
+    print(frequencies(*alleles[x:x+4]))
     print(likelihoods(*alleles[x:x+4]))
     print(likelihoods4(*alleles[x:x+4]))
 
